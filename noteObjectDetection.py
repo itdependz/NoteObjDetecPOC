8a72368cfa08bb05251a42e6ad53abd438afd149
Samarth <samarthdas81@gmail.com>
2025-06-26 17:01:30 -0400

    created video

--- noteObjectDetection.py
+++ noteObjectDetection.py
@@ -4,11 +4,9 @@ import cv2
 import numpy as np
 import json
 
-rf = Roboflow(api_key="PHdCfHHJVoVwM0ee9rP7")
+rf = Roboflow(api_key="v6L7b4dceS02WW7pcCUx")
 project = rf.workspace().project("2024-frc")
 model = project.version(8).model
-
-model.download()
 
 video_path = "2024-frc-robot-pov.mp4"
 cap = cv2.VideoCapture(video_path)
@@ -16,7 +14,7 @@ cap = cv2.VideoCapture(video_path)
 #Output video setup
 output_path = "NoteDetections.mp4"
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-out = cv2.VideoWriter(output_path, fourcc, 0, (640, 480))
+out = cv2.VideoWriter(output_path, fourcc, 30, (640, 480))
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -33,11 +31,11 @@ while cap.isOpened():
         h = int(prediction['height'])
         class_name = prediction['class']
         confidence = prediction['confidence']
-        if class_name=="Note":
-            class_name = "Note Detected"
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(frame, f'{class_name} {confidence:.2f}', (x, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+        
+        if class_name=="note":
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(frame, f'{class_name} {confidence:.2f}', (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
 
     cv2.imshow('Detection', frame)
     out.write(frame)
